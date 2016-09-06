dac6be44809b9e260133f7d45868758ae0935eea
nicklocascio <nicoloc4545@gmail.com>
2016-09-06 09:08:42 -0600

    Only have a few notes so far

---
+++ Boot Camp Array Challenges/src/arrayChallenges.java
@@ -0,0 +1,16 @@
+
+public class arrayChallenges
+	{
+	public static void main(String[] args)
+		{
+		//int num = 45453
+		//int digit = num%10
+		//int digit = num/10
+		//Put in loop for #4
+		//for(String moose : studentList
+//			{
+//			System.out.println(moose);		
+//			}
+		//That is a for each loop
+		}
+	}

74ae0e012f926acae760690884608b4a5d3adc4a
nicklocascio <nicoloc4545@gmail.com>
2016-09-08 09:09:11 -0600

    Still needs work

--- Boot Camp Array Challenges/src/arrayChallenges.java
+++ Boot Camp Array Challenges/src/arrayChallenges.java
@@ -12,15 +12,23 @@ public class arrayChallenges
 		}
 	public static void main(String[] args)
 		{
-		// FIRST ONE
-		// ASK THE TYPE OF SWEET
+//		 FIRST ONE
+//		 ASK THE TYPE OF SWEET
+			
 //		Scanner userInput = new Scanner(System.in);
 //		System.out.println("How many favorite sweets do you have?");
-//		int favoriteSweets = userInput.nextInt();
+//		int numberOfFavoriteSweets = userInput.nextInt();
+//		Scanner moreUserInput = new Scanner(System.in);
+//		for(int i = 0; i < numberOfFavoriteSweets; i++)
+//			{
+//			System.out.println("Name one of them.");
+//			String favoriteSweets = moreUserInput.nextLine();
+//			}		
+//		String[] sweets = new String[numberOfFavoriteSweets];			
+		
 //		for(int i = 0; i < favoriteSweets; i++)
 //			{	
-//			int[]sweets;
-//			sweets = new int[i];				
+//			int[]sweets = new int[i];				
 //			for(int hello : sweets)
 //				{
 //				System.out.println(hello);
@@ -28,47 +36,47 @@ public class arrayChallenges
 //			}
 			
 		// SECOND ONE
-		inputNumber();
-		int[]numbers;
-		numbers = new int[8];
-		// MAKE THIS IN A FOR LOOP
-		numbers[0] = number;
-		inputNumber();
-		numbers[1] = number;
-		inputNumber();
-		numbers[2] = number;
-		inputNumber();
-		numbers[3] = number;
-		inputNumber();
-		numbers[4] = number;
-		inputNumber();
-		numbers[5] = number;
-		inputNumber();
-		numbers[6] = number;
-		inputNumber();
-		numbers[7] = number;
-		int evenCounter = 0;
-		int oddCounter = 0;
-		//
-		for(int i = 0; i < numbers.length; i++)
-			{
-			if(i%2 == 0)
-				{
-				evenCounter = evenCounter + number;
-				}
-			else
-				{
-				oddCounter = oddCounter + number;
-				}
-			System.out.println(evenCounter);
-			System.out.println(oddCounter);
+//		inputNumber();
+//		int[]numbers;
+//		numbers = new int[8];
+//		// MAKE THIS IN A FOR LOOP
+//		numbers[0] = number;
+//		inputNumber();
+//		numbers[1] = number;
+//		inputNumber();
+//		numbers[2] = number;
+//		inputNumber();
+//		numbers[3] = number;
+//		inputNumber();
+//		numbers[4] = number;
+//		inputNumber();
+//		numbers[5] = number;
+//		inputNumber();
+//		numbers[6] = number;
+//		inputNumber();
+//		numbers[7] = number;
+//		int evenCounter = 0;
+//		int oddCounter = 0;
+//		//
+//		for(int i = 0; i < numbers.length; i++)
+//			{
+//			if(i%2 == 0)
+//				{
+//				evenCounter = evenCounter + number;
+//				}
+//			else
+//				{
+//				oddCounter = oddCounter + number;
+//				}
+//			System.out.println(evenCounter);
+//			System.out.println(oddCounter);
 			}
 //		for(int wassup : numbers)
 //			{
 //			System.out.println(wassup);
 //			}
 		}
-	}
+//	}
 
 
 

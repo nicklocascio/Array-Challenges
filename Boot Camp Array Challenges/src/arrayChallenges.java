230727f16c0afb27d6b74a4e4b751228b91ad63c
nicklocascio <nicklocascio@192.168.0.13>
2016-09-08 19:58:06 -0600

    Almost done just a few minor tweaks that I don't know

--- Boot Camp Array Challenges/src/arrayChallenges.java
+++ Boot Camp Array Challenges/src/arrayChallenges.java
@@ -10,73 +10,101 @@ public class arrayChallenges
 		number = userInput.nextInt();
 		return number;
 		}
+	static int randomNumber;
+	public static int randomNumberGenerator()
+		{
+		randomNumber = (int)(Math.random()*90)+10;
+		return randomNumber;
+		}
 	public static void main(String[] args)
 		{
-//		 FIRST ONE
-//		 ASK THE TYPE OF SWEET
-			
+		// FIRST ONE
+		// DONE
 //		Scanner userInput = new Scanner(System.in);
 //		System.out.println("How many favorite sweets do you have?");
 //		int numberOfFavoriteSweets = userInput.nextInt();
+//		String[] sweets = new String[numberOfFavoriteSweets];			
 //		Scanner moreUserInput = new Scanner(System.in);
 //		for(int i = 0; i < numberOfFavoriteSweets; i++)
 //			{
-//			System.out.println("Name one of them.");
-//			String favoriteSweets = moreUserInput.nextLine();
-//			}		
-//		String[] sweets = new String[numberOfFavoriteSweets];			
-		
-//		for(int i = 0; i < favoriteSweets; i++)
-//			{	
-//			int[]sweets = new int[i];				
-//			for(int hello : sweets)
+//			if(i == 0)
 //				{
-//				System.out.println(hello);
+//				System.out.println("Name the first one.");
 //				}
+//			else
+//				{
+//				System.out.println("Enter another.");
+//				}
+//			sweets[i] = moreUserInput.nextLine();
+//			}
+//		System.out.println("Your favorite sweets are: ");
+//		for(String hello : sweets)
+//			{				
+//			System.out.println(hello);
 //			}
 			
 		// SECOND ONE
-//		inputNumber();
-//		int[]numbers;
-//		numbers = new int[8];
-//		// MAKE THIS IN A FOR LOOP
-//		numbers[0] = number;
-//		inputNumber();
-//		numbers[1] = number;
-//		inputNumber();
-//		numbers[2] = number;
-//		inputNumber();
-//		numbers[3] = number;
-//		inputNumber();
-//		numbers[4] = number;
-//		inputNumber();
-//		numbers[5] = number;
-//		inputNumber();
-//		numbers[6] = number;
-//		inputNumber();
-//		numbers[7] = number;
+		// JUST FIGURE OUT COUNTER
 //		int evenCounter = 0;
 //		int oddCounter = 0;
-//		//
+//		int[]numbers = new int[8];
+//		for(int i = 0; i < 8; i++)
+//			{
+//			inputNumber();
+//			numbers[i] = number;
+//			}
+//		
 //		for(int i = 0; i < numbers.length; i++)
 //			{
 //			if(i%2 == 0)
 //				{
-//				evenCounter = evenCounter + number;
+//				evenCounter += i;
 //				}
 //			else
 //				{
-//				oddCounter = oddCounter + number;
-//				}
-//			System.out.println(evenCounter);
-//			System.out.println(oddCounter);
-			}
+//				oddCounter += i;
+//				}			
+//			}
+//		System.out.println(evenCounter);
+//		System.out.println(oddCounter);
 //		for(int wassup : numbers)
 //			{
 //			System.out.println(wassup);
 //			}
+		
+		// THIRD ONE	
+		// ONLY NEED TO KNOW HOW TO INSERT BACKWARDS
+//		int[]randomNumbers = new int[5];
+//		for(int i = 0; i < 5; i++)
+//			{
+//			randomNumberGenerator();
+//			System.out.println(randomNumber);
+//			randomNumbers[i] = randomNumber;
+//			}
+//		for(int heyo : randomNumbers)
+//			{
+//			System.out.println(heyo);
+//			}
+		
+		// FOURTH ONE
+		// JUST NEED TO KNOW HOW TO INSERT BACKWARDS
+		int[]fourDigits = new int[4];
+		Scanner userInput3 = new Scanner(System.in);
+		System.out.println("Please enter a four digit number.");
+		int fourDigitNumber = userInput3.nextInt();
+		for(int i = 0; i < 4; i++)
+			{
+			fourDigits[i] = fourDigitNumber%10;			
+			fourDigitNumber = fourDigitNumber / 10;			
+			}
+		for(int ay : fourDigits)
+			{
+			System.out.println(ay);
+			}		
 		}
-//	}
+	}
+		
+	
 
 
 

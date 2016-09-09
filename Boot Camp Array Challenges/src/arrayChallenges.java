1eddabbf75fb601d7c6aaa2d39c6ce7a78d551ee
nicklocascio <nicoloc4545@gmail.com>
2016-09-09 09:09:19 -0600

    Done! Add one thing for Mr. McGuire in first challenge this weekend.

--- Boot Camp Array Challenges/src/arrayChallenges.java
+++ Boot Camp Array Challenges/src/arrayChallenges.java
@@ -19,75 +19,69 @@ public class arrayChallenges
 	public static void main(String[] args)
 		{
 		// FIRST ONE
-		// DONE
-//		Scanner userInput = new Scanner(System.in);
-//		System.out.println("How many favorite sweets do you have?");
-//		int numberOfFavoriteSweets = userInput.nextInt();
-//		String[] sweets = new String[numberOfFavoriteSweets];			
-//		Scanner moreUserInput = new Scanner(System.in);
-//		for(int i = 0; i < numberOfFavoriteSweets; i++)
-//			{
-//			if(i == 0)
-//				{
-//				System.out.println("Name the first one.");
-//				}
-//			else
-//				{
-//				System.out.println("Enter another.");
-//				}
-//			sweets[i] = moreUserInput.nextLine();
-//			}
-//		System.out.println("Your favorite sweets are: ");
-//		for(String hello : sweets)
-//			{				
-//			System.out.println(hello);
-//			}
+		// ADD DIFFERENT QUESTIONS BASED ON SWEETS
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("How many favorite sweets do you have?");
+		int numberOfFavoriteSweets = userInput.nextInt();
+		String[] sweets = new String[numberOfFavoriteSweets];			
+		Scanner moreUserInput = new Scanner(System.in);
+		boolean namingSweets = true;
+		for(int i = 0; i < numberOfFavoriteSweets; i++)
+			{
+			if(i == 0)
+				{
+				System.out.println("Name the first sweet.");
+				}
+			// ADD HERE
+			sweets[i] = moreUserInput.nextLine();
+			}
+		System.out.println("Your favorite sweets are: ");
+		for(String hello : sweets)
+			{				
+			System.out.println(hello);
+			}
 			
 		// SECOND ONE
-		// JUST FIGURE OUT COUNTER
-//		int evenCounter = 0;
-//		int oddCounter = 0;
-//		int[]numbers = new int[8];
-//		for(int i = 0; i < 8; i++)
-//			{
-//			inputNumber();
-//			numbers[i] = number;
-//			}
-//		
-//		for(int i = 0; i < numbers.length; i++)
-//			{
-//			if(i%2 == 0)
-//				{
-//				evenCounter += i;
-//				}
-//			else
-//				{
-//				oddCounter += i;
-//				}			
-//			}
-//		System.out.println(evenCounter);
-//		System.out.println(oddCounter);
-//		for(int wassup : numbers)
-//			{
-//			System.out.println(wassup);
-//			}
+		int evenCounter = 0;
+		int oddCounter = 0;
+		int[]numbers = new int[8];
+		System.out.println("You will be asked to input eight numbers.");
+		for(int i = 0; i < 8; i++)
+			{
+			inputNumber();
+			numbers[i] = number;
+			}
+		
+		for(int i = 0; i < numbers.length; i++)
+			{
+			if(i%2 == 0)
+				{
+				evenCounter += numbers[i];
+				}
+			else
+				{
+				oddCounter += numbers[i];
+				}			
+			}
+		System.out.println("The sum of the even numbers is: " + evenCounter);
+		System.out.println("The sum of the odd numbers is: " + oddCounter);
 		
 		// THIRD ONE	
-		// ONLY NEED TO KNOW HOW TO INSERT BACKWARDS
-//		int[]randomNumbers = new int[5];
-//		for(int i = 0; i < 5; i++)
-//			{
-//			randomNumberGenerator();
-//			System.out.println(randomNumber);
-//			randomNumbers[i] = randomNumber;
-//			}
-//		for(int heyo : randomNumbers)
-//			{
-//			System.out.println(heyo);
-//			}
+		int[]randomNumbers = new int[5];
+		System.out.println("The random numbers are: ");
+		for(int i = 0; i < 5; i++)
+			{
+			randomNumberGenerator();
+			System.out.println(randomNumber);
+			randomNumbers[4 - i] = randomNumber;
+			}	
+		System.out.println("The reversed random numbers are: ");
+		for(int heyo : randomNumbers)
+			{
+			System.out.println(heyo);
+			}
 		
 		// FOURTH ONE
-		// JUST NEED TO KNOW HOW TO INSERT BACKWARDS
 		int[]fourDigits = new int[4];
 		Scanner userInput3 = new Scanner(System.in);
 		System.out.println("Please enter a four digit number.");
@@ -97,24 +91,10 @@ public class arrayChallenges
 			fourDigits[i] = fourDigitNumber%10;			
 			fourDigitNumber = fourDigitNumber / 10;			
 			}
+		System.out.println("The stripped digits in reverse order are: ");
 		for(int ay : fourDigits)
 			{
-			System.out.println(ay);
+			System.out.print(ay);
 			}		
 		}
 	}
-		
-	
-
-
-
-//NOTES
-//int num = 45453
-//int digit = num%10
-//int digit = num/10
-//Put in loop for #4
-//for(String moose : studentList
-//	{
-//	System.out.println(moose);		
-//	}
-//That is a for each loop
